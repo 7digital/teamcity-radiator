d13e75299d483ed98c67fde568d24f1431152c59
rhargreaves <robert.hargreaves@7digital.com>
2017-08-17 17:23:45 +0100

    Remove commented-out code

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ var debug = require('debug')('app');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
-
 var routes = require('./routes');
 
 var app = express();
@@ -16,11 +12,6 @@ app.set('view engine', 'jade');
 
 app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded());
-// app.use(cookieParser());
-
 app.use(routes);
 
 /// catch 404 and forward to error handler
@@ -31,10 +22,6 @@ app.use(function (req, res, next) {
 });
 
 /// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -42,17 +29,6 @@ app.use(function (err, req, res, next) {
     error: err
   });
 });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
 
 process.on('uncaughtException', function (err) {
   debug('Caught exception: ' + err);
